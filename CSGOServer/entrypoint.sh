a5709b4e0c0e8cb52635d0af26891ee964d786e6
Anthony <harkin1987@gmail.com>
2022-09-24 18:50:50 +0100

    Added server launching

--- CSGOServer/entrypoint.sh
+++ CSGOServer/entrypoint.sh
@@ -3,5 +3,5 @@
 #Download/Update game files
 bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${GAMEDIR}" +login anonymous +app_update "${APPID}" +quit
 #Launch Server
-
+bash "${GAMEDIR}/srcds_linux"
 #srcds -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
