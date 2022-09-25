05ad20b3f1774c9985ad7662926a8aeea8f65b00
Anthony <harkin1987@gmail.com>
2022-09-25 08:58:42 +0100

    Replaced wget with COPY for entrypoint

--- CSGOServer/entrypoint/entrypoint.sh
+++ CSGOServer/entrypoint/entrypoint.sh
@@ -4,4 +4,3 @@
 bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${GAMEDIR}" +login anonymous +app_update "${APPID}" +quit
 #Launch Server
 ./srcds_run
-#srcds -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
